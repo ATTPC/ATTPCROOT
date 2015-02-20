04a368a14206c2378a47913b31f208e7454300f7
Yassid <yassid.ayyad@gmail.com>
2015-02-20 19:21:11 +0900

    Added multi_array structure for AtMap class

--- AtMap/AtTpcMap.cxx
+++ AtMap/AtTpcMap.cxx
@@ -8,16 +8,36 @@
 #include "AtTpcMap.h"
 
 #include <iostream>
+#include <cassert>
+
 using std::cout;
 using std::endl;
 
 
-AtTpcMap::AtTpcMap()
+AtTpcMap::AtTpcMap():AtPadCoord(boost::extents[10240][3][2])
 {
+ Initialize();
 }
 
 AtTpcMap::~AtTpcMap()
 {
+}
+
+void AtTpcMap::Initialize()
+{
+ 
+  std::fill( AtPadCoord.data(), AtPadCoord.data()+AtPadCoord.num_elements() , 0);
+  std::cout<<" ATTPC Pad Coordinates container initialized "<<endl;
+}
+
+void AtTpcMap::Dump(){
+	
+    int values = 0;
+  	 for(index i = 0; i != 10240; ++i) 
+   	   for(index j = 0; j != 3; ++j)
+     	     for(index k = 0; k != 2; ++k)
+             	std::cout<<AtPadCoord[i][j][k]<<endl;
+
 }
 
 
